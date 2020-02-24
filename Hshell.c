d8ed2ac42d0d05f39eefe9320e8bbb81dce7661b
SUSTechHong <11612425@mail.sustech.edu.cn>
2020-02-24 14:38:49 +0800

    initialize

---
+++ Hshell.c
@@ -0,0 +1,23 @@
+#include "Hshell.h"
+
+void init()
+{
+    
+}
+
+void main_loop()
+{
+    while (loop_status)
+    {
+        
+    }
+}
+
+int main()
+{
+    init();
+
+    main_loop();
+
+    return 0;
+}
---
+++ Hshell.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+bool loop_status = 1;
+
+void init();
+void main_loop();
